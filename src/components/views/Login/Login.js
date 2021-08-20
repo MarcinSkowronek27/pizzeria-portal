dba5e143e7d78d3f51e71d19db38255a138d4cc4
Marcin Skowronek <marcinfoto35@op.pl>
2021-08-20 22:17:46 +0200

    Prepare Tables component

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import styles from './login.modules.scss';
-import PropTypes from 'prop-types';
 
-const Login = props => {
+const Login = () => {
   <div className={styles.component}>
     <h2>
       Login view
     </h2>
   </div>;
-};
-
-Login.propTypes = {
-  children: PropTypes.node,
 };
 
 export default Login;
