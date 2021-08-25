ab082984269294ef2d52a10df0f4ab0bc179c3cf
Marcin Skowronek <marcinfoto35@op.pl>
2021-08-25 11:48:20 +0200

    input Login in Login component

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import styles from './Login.scss';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Toolbar from '@material-ui/core/Toolbar';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
 
 const Login = () => (
   <div className={styles.component}>
-    <h2>
-      Login view
-    </h2>
+    <Container>
+      <Toolbar />
+      <FormControl className={styles.margin}>
+        <InputLabel htmlFor="input-with-icon-login">Login</InputLabel>
+        <Input
+          id="input-with-icon-adornment"
+          startAdornment={
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+      <Button>Login</Button>
+    </Container>
+
   </div>
 );
 
