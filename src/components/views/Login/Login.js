09e18a82f5ca6d062a81f139dcd5b9f184a8f90b
Marcin Skowronek <marcinfoto35@op.pl>
2021-08-25 15:25:38 +0200

    Work on login component

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -8,26 +8,72 @@ import AccountCircle from '@material-ui/icons/AccountCircle';
 import Toolbar from '@material-ui/core/Toolbar';
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import IconButton from '@material-ui/core/IconButton';
 
-const Login = () => (
-  <div className={styles.component}>
-    <Container>
-      <Toolbar />
-      <FormControl className={styles.margin}>
-        <InputLabel htmlFor="input-with-icon-login">Login</InputLabel>
-        <Input
-          id="input-with-icon-adornment"
-          startAdornment={
-            <InputAdornment position="start">
-              <AccountCircle />
-            </InputAdornment>
-          }
-        />
-      </FormControl>
-      <Button>Login</Button>
-    </Container>
+const Login = () => {
+  const [values, setValues] = React.useState({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
+  });
 
-  </div>
-);
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className={styles.component}>
+      <Container maxWidth='lg'>
+        <Toolbar />
+        <FormControl className={styles.margin}>
+          <InputLabel htmlFor="input-with-icon-login">Login</InputLabel>
+          <Input
+            id="input-with-icon-adornment"
+            startAdornment={
+              <InputAdornment position="start">
+                <AccountCircle />
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+      </Container>
+      <Container maxWidth='lg' >
+        <FormControl className={styles.login}>
+          <InputLabel  htmlFor="standard-adornment-password">Password</InputLabel>
+          <Input className={styles.login}
+            id="standard-adornment-password"
+            type={values.showPassword ? 'text' : 'password'}
+            value={values.password}
+            onChange={handleChange('password')}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                >
+                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+      </Container>
+      <Button className={styles.login}>Login</Button>
+    </div>
+  );
+};
 
 export default Login;
