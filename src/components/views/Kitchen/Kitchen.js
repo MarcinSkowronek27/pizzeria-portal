104e255a47a4079312c9c68bd24e4a2307bb6678
Marcin Skowronek <marcinfoto35@op.pl>
2021-08-26 14:28:27 +0200

    DONE kitchen component with Material UI

--- src/components/views/Kitchen/Kitchen.js
+++ src/components/views/Kitchen/Kitchen.js
@@ -1,62 +1,138 @@
 import React from 'react';
 import styles from './Kitchen.module.scss';
-import { DataGrid } from '@material-ui/data-grid';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Checkbox from '@material-ui/core/Checkbox';
+import { green } from '@material-ui/core/colors';
+import { withStyles } from '@material-ui/core/styles';
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
-  {
-    field: 'firstName',
-    headerName: 'First name',
-    width: 150,
-    editable: true,
+const GreenCheckbox = withStyles({
+  root: {
+    color: green[400],
+    '&$checked': {
+      color: green[600],
+    },
   },
-  {
-    field: 'lastName',
-    headerName: 'Last name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 110,
-    editable: true,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.getValue(params.id, 'firstName') || ''} ${params.getValue(params.id, 'lastName') || ''
-      }`,
-  },
-];
+  checked: {},
+})(props => <Checkbox color="default" {...props} />);
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+const demoContent = [
+  {
+    orderTime: '14:00',
+    id: '1',
+    order: 4587,
+    dishes: ['Greek Pizza', 'Tomato Salad'],
+  },
+  {
+    orderTime: '14:30',
+    id: '2',
+    order: 5675,
+    dishes: ['Four Cheese', 'Coke'],
+  },
+  {
+    orderTime: '14:45',
+    id: '3',
+    order: 6889,
+    dishes: ['Bruschetta', 'Spaghetti Bolognese', 'Caffe Latte'],
+  },
+  {
+    orderTime: '15:30',
+    id: '4',
+    order: 7891,
+    dishes: ['Farmer Burger', 'Tea', 'Water'],
+  },
+  {
+    orderTime: '16:00',
+    id: '2',
+    order: 8912,
+    dishes: ['Hot Spicy Burger', 'Coffee'],
+  },
+  {
+    orderTime: '16:00',
+    id: '3',
+    order: 9123,
+    dishes: ['Margerita Pizza', 'Orange Juice'],
+  },
+  {
+    orderTime: '16:10',
+    id: '4',
+    order: 1245,
+    dishes: ['Buffalo Wings', 'Canlis Salad'],
+  },
+  {
+    orderTime: '16:30',
+    id: '5',
+    order: 2356,
+    dishes: ['Carnitas Burrito', 'Cheese Slice'],
+  },
+  {
+    orderTime: '18:20',
+    id: 'online',
+    order: 5856,
+    dishes: ['Carnitas Burrito', 'Cheese Slice'],
+  },
+  {
+    orderTime: '19:00',
+    id: '6',
+    order: 1234,
+    dishes: ['Margherita', 'Coke', 'Apple Juice'],
+  },
+  {
+    orderTime: '21:30',
+    id: 'online',
+    order: 2345,
+    dishes: ['New York-Style Pizza', 'Pepsi'],
+  },
+  {
+    orderTime: '21:30',
+    id: '5',
+    order: 3456,
+    dishes: ['Sicilian Pizza', 'Water'],
+  },
 ];
 
 const Kitchen = () => (
   <div className={styles.component}>
-    <div className={styles.hab}>gdfsgdfsg</div>
-    <DataGrid className={styles.component}
-      rows={rows}
-      columns={columns}
-      pageSize={5}
-      checkboxSelection
-      disableSelectionOnClick
-    />
+    <h2>Kitchen</h2>
+    <Paper>
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell>Order Time</TableCell>
+            <TableCell>Table</TableCell>
+            <TableCell>Order</TableCell>
+            <TableCell>Dishes</TableCell>
+            <TableCell>Completed</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {demoContent.map(row => (
+            <TableRow key={row.order}>
+              <TableCell>{row.orderTime}</TableCell>
+              <TableCell component="th" scope="row">
+                {row.id}
+              </TableCell>
+              <TableCell>{row.order}</TableCell>
+              <TableCell>
+                {row.dishes.map(dish => (
+                  <p key={dish}>{dish}</p>
+                ))}
+              </TableCell>
+              <TableCell>
+                <GreenCheckbox
+                  value="completed"
+                  inputProps={{ 'aria-label': 'completed' }}
+                />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
   </div>
 );
 
